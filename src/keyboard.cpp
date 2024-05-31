75b0562c359bf1b0e368efaac60c0ee03e9f9767
PsyDark00 <barros.ericles@outlook.com>
2024-05-31 16:15:23 -0300

    modificação das teclas de movimento

--- src/keyboard.cpp
+++ src/keyboard.cpp
@@ -1,5 +1,6 @@
 #include <GL/glut.h>
 #include <screen.h>
+#include <physics.h>
 
 int pause = 0;
 GLfloat translateX = 0, translateY = 0;
@@ -20,20 +21,20 @@ void teclado(unsigned char tecla, int x, int y)
 
     case 'w':
     case 'W':
-        translateY += 10;
+        //translateY += 10;
         break;
     case 's':
     case 'S':
-        translateY -= 10;
+        //translateY -= 10;
         break;
     case 'a':
     case 'A':
-        translateX -= 10;
+        //translateX -= 10;
         break;
 
     case 'd':
     case 'D':
-        translateX += 10;
+        //translateX += 10;
         break;
     case 27: // Tecla ESC
         exit(0);
@@ -47,16 +48,17 @@ void teclasEspeciais(int tecla, int x, int y)
     switch (tecla)
     {
     case GLUT_KEY_UP:
-        translateY += 10;
+        //translateY += 10;
         break;
     case GLUT_KEY_LEFT:
-        translateX -= 10;
+        if(translateX > -larguraJanela/2){
+            translateX -= 10;
+        }
         break;
     case GLUT_KEY_RIGHT:
-        translateX += 10;
-        break;
-    case GLUT_KEY_DOWN:
-        translateY -= 10;
+        if(translateX < larguraJanela/2){
+            translateX += 10;
+        }
         break;
     }
     glutPostRedisplay();
--- src/physics.cpp
+++ src/physics.cpp
@@ -1,4 +1,5 @@
 #include <GL/glut.h>
+#include <keyboard.h>
 
 void colisao(int xObj1, int yObj1, int xObj2, int yObj2){
 
